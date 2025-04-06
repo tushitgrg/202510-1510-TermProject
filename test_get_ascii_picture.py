58129e17caf673f49ffbca3cf69739fc7ace465b
Tushit <tushitgarg123@gmail.com>
2025-04-05 21:48:01 -0700

    Replace the unit and doctest for get_ascii_picture

--- test_get_ascii_picture.py
+++ test_get_ascii_picture.py
@@ -4,7 +4,71 @@ from jigsaw import get_ascii_picture
 
 
 class Test(TestCase):
-    def test_get_ascii_picture_length(self):
-        expected = 9
-        actual = len(get_ascii_picture())
+    def test_get_ascii_picture_(self):
+        expected = [
+            [
+                "     ",
+                "  _  ",
+                " / \\_",
+                "/    ",
+                "|    "
+            ],
+            [
+                "     ",
+                "___  ",
+                "   \\_",
+                "     ",
+                "     "
+            ],
+            [
+                "     ",
+                "     ",
+                "_    ",
+                " \\   ",
+                "  \\  "
+            ],
+            [
+                "|    ",
+                "|    ",
+                "| O  ",
+                "|    ",
+                "\\    "
+            ],
+            [
+                "     ",
+                "     ",
+                "     ",
+                "     ",
+                "     "
+            ],
+            [
+                "     ",
+                "     ",
+                "  O  ",
+                "     ",
+                "    /"
+            ],
+            [
+                " \\   ",
+                "  \\  ",
+                "   \\ ",
+                "    \\",
+                "     "
+            ],
+            [
+                "     ",
+                "     ",
+                "     ",
+                "\\___/",
+                "     "
+            ],
+            [
+                "   / ",
+                "  /  ",
+                " /   ",
+                "/    ",
+                "     "
+            ]
+        ]
+        actual = get_ascii_picture()
         self.assertEqual(expected, actual)
